ada777274fb5eb9f6465aa241047082c2e676d9b
veter391 <nazarcova391@gmail.com>
2023-02-14 20:40:50 +0100

    js fix

--- script.js
+++ script.js
@@ -1,1 +1,121 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('"B A";z.n(\'y\',()=>{7 9=C(\'Введи свое имя! \',\'\').D().G();7 r=3.8(\'F\'),c=3.8(\'.c\');b l=3.E(".x");6(9!==\'кира\'&&9!==\'u\'){3.8(\'p\').v=`Жизнь - она как карамель,  С разною начинкою!.  У одних — так чистый мед,  У других — с горчинкою. $Ну бывает — не идет, Бывает — не получается.  Или к стеночки припрет,  С каждым ведь случается.  $Все равно ее люби,  И цени мгновения!.  Пожелаю я тебе,  Супер - настроения!.`}r.n(\'w\',(e)=>{c.t.H(\'K\');q(()=>{l.m(1=>{1.4.j=\'h\';1.4.W=\'T\';b o=1.a.S("");1.a="";b d=U;6(1.t.V(\'I\')){d=0}q(()=>{o.m((5,i)=>{7 2=3.k("2");7 g=3.k("g");6(5===\'$\'){2.4.j=\'h\';2.4.P=\'Q\';1.f(2)}J{2.a=5;2.4.L=`${i*0.M}s`;1.f(2);6(5===\',\'||5===\'.\'){1.f(g)}}})},d)})},O);e.N.R()})});',59,59,'|glowInText|span|document|style|letter|if|const|querySelector|pass|textContent|let|wrapper|delay||append|br|block||display|createElement|glowInTexts|forEach|addEventListener|letters||setTimeout|btn||classList|kira|innerHTML|click|glowIn|DOMContentLoaded|window|strict|use|prompt|toLowerCase|querySelectorAll|button|trim|add|fast|else|active|animationDelay|05|target|1000|marginBottom|10px|remove|split|visible|1200|contains|visibility'.split('|'),0,{}))
+// eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('"B A";z.n(\'y\',()=>{7 9=C(\'Введи свое имя! \',\'\').D().G();7 r=3.8(\'F\'),c=3.8(\'.c\');b l=3.E(".x");6(9!==\'кира\'&&9!==\'u\'){3.8(\'p\').v=`Жизнь - она как карамель,  С разною начинкою!.  У одних — так чистый мед,  У других — с горчинкою. $Ну бывает — не идет, Бывает — не получается.  Или к стеночки припрет,  С каждым ведь случается.  $Все равно ее люби,  И цени мгновения!.  Пожелаю я тебе,  Супер - настроения!.`}r.n(\'w\',(e)=>{c.t.H(\'K\');q(()=>{l.m(1=>{1.4.j=\'h\';1.4.W=\'T\';b o=1.a.S("");1.a="";b d=U;6(1.t.V(\'I\')){d=0}q(()=>{o.m((5,i)=>{7 2=3.k("2");7 g=3.k("g");6(5===\'$\'){2.4.j=\'h\';2.4.P=\'Q\';1.f(2)}J{2.a=5;2.4.L=`${i*0.M}s`;1.f(2);6(5===\',\'||5===\'.\'){1.f(g)}}})},d)})},O);e.N.R()})});',59,59,'|glowInText|span|document|style|letter|if|const|querySelector|pass|textContent|let|wrapper|delay||append|br|block||display|createElement|glowInTexts|forEach|addEventListener|letters||setTimeout|btn||classList|kira|innerHTML|click|glowIn|DOMContentLoaded|window|strict|use|prompt|toLowerCase|querySelectorAll|button|trim|add|fast|else|active|animationDelay|05|target|1000|marginBottom|10px|remove|split|visible|1200|contains|visibility'.split('|'),0,{}))
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('"F D";C.r(\'B\',()=>{d 3=z(\'Введи свое имя! \',\'\').A().E();d q=4.8(\'M\'),l=4.8(\'.l\');f u=4.G(".K");7(3===\'кира\'||3===\'o\'){2.1(\'-------------------------------\');2.1();2.1(`I H ${3}`);2.1();2.1(\'-------------------------------\')}c 7(3===\'карина\'||3===\'J\'){2.1(\'-------------------------------\');2.1();2.1(`Бее ее это-же ${3}`);2.1();2.1(\'-------------------------------\');4.8(\'p\').a=``;4.8(\'p\').a=`---------------------------------------- $Кариша - кариша, Маленький ты панк. В жизни возвращается, Всё как бумеранг. $И сейчас услышишь, То что сказала мне. Совсем я не противный, И сама ты бе еее! $---------------------------------------`}c 7(3===\'L\'||3===\'ваня\'){2.1(\'-------------------------------\');2.1();2.1(`Я + ${3} = никогда ни за что`);2.1(`Карина + ${3} = ( ˘⌣˘)♡(˘⌣˘ )`);2.1();2.1(\'-------------------------------\');4.8(\'p\').a=``;4.8(\'p\').a=`---------------------------------------- $Я + ${3} = никогда ни за что $Карина + ${3} = ( ˘⌣˘)♡(˘⌣˘ ) $---------------------------------------`}c 7(3!==\'кира\'&&3!==\'o\'){4.8(\'p\').a=`Жизнь - она как карамель,  С разною начинкою!.  У одних — так чистый мед,  У других — с горчинкою. $Ну бывает — не идет, Бывает — не получается.  Или к стеночки припрет,  С каждым ведь случается.  $Все равно ее люби,  И цени мгновения!.  Пожелаю я тебе,  Супер - настроения!.`}q.r(\'10\',(e)=>{l.n.12(\'X\');m(()=>{u.w(5=>{5.9.x=\'v\';5.9.Y=\'Z\';f t=5.g.11("");5.g="";f h=N;7(5.n.U(\'V\')){h=0}m(()=>{t.w((b,i)=>{d 6=4.y("6");d j=4.y("j");7(b===\'$\'){6.9.x=\'v\';6.9.P=\'O\';5.k(6)}c{6.g=b;6.9.Q=`${i*0.R}s`;5.k(6);7(b===\',\'||b===\'.\'){5.k(j)}}})},h)})},T);e.S.W()})});',62,65,'|log|console|pass|document|glowInText|span|if|querySelector|style|innerHTML|letter|else|const||let|textContent|delay||br|append|wrapper|setTimeout|classList|kira||btn|addEventListener||letters|glowInTexts|block|forEach|display|createElement|prompt|toLowerCase|DOMContentLoaded|window|strict|trim|use|querySelectorAll|you|Love|karina|glowIn|ivan|button|1200|10px|marginBottom|animationDelay|05|target|1000|contains|fast|remove|active|visibility|visible|click|split|add'.split('|'),0,{}))
+
+// "use strict";
+
+// window.addEventListener('DOMContentLoaded', () => {
+
+//   const pass = prompt('Введи свое имя! ', '').toLowerCase().trim();
+//   const btn = document.querySelector('button'),
+//         wrapper = document.querySelector('.wrapper');
+//   let glowInTexts = document.querySelectorAll(".glowIn");
+  
+//   if (pass === 'кира' || pass === 'kira' ) {
+//     console.log('-------------------------------');
+//     console.log();
+//     console.log(`Love you ${pass}`);
+//     console.log();
+//     console.log('-------------------------------');
+//   } else if (pass === 'карина' || pass === 'karina'){
+//     console.log('-------------------------------');
+//     console.log();
+//     console.log(`Бее ее это-же ${pass}`);
+//     console.log();
+//     console.log('-------------------------------');
+//     document.querySelector('p').innerHTML = ``;
+
+//     document.querySelector('p').innerHTML = `
+//     ----------------------------------------
+//     $Кариша - кариша,
+//     Маленький ты панк.
+//     В жизни возвращается,
+//     Всё как бумеранг.
+
+//     $И сейчас услышишь,
+//     То что сказала мне.
+//     Совсем я не противный,
+//     И сама ты бее ее!
+//     $---------------------------------------
+//     `;
+//   } else if (pass === 'ivan' || pass === 'ваня'){
+//     console.log('-------------------------------');
+//     console.log();
+
+//     console.log(`Я + ${pass} = никогда ни за что`);
+//     console.log(`Карина + ${pass} = ( ˘⌣˘)♡(˘⌣˘ )`);
+
+//     console.log();
+//     console.log('-------------------------------');
+//     document.querySelector('p').innerHTML = ``;
+
+//     document.querySelector('p').innerHTML = `
+//     ----------------------------------------
+//     $Я + ${pass} = никогда ни за что
+//     $Карина + ${pass} = ( ˘⌣˘)♡(˘⌣˘ )
+//     $---------------------------------------
+//     `;
+//   } else if (pass !== 'кира' && pass !== 'kira' ) {
+//     document.querySelector('p').innerHTML = `
+//     Жизнь - она как карамель, 
+//     С разною начинкою!.  
+//     У одних — так чистый мед, 
+//     У других — с горчинкою.  
+//     $Ну бывает — не идет, 
+//     Бывает — не получается.  
+//     Или к стеночки припрет,  
+//     С каждым ведь случается.  
+//     $Все равно ее люби, 
+//     И цени мгновения!.  
+//     Пожелаю я тебе, 
+//     Супер-настроения!.  
+//     `;
+//   // ♥ https://smsta.ru/poems/positive
+//   } 
+
+//   btn.addEventListener('click' , (e) => {
+//     wrapper.classList.add('active');
+
+//     setTimeout(() => {
+//       glowInTexts.forEach(glowInText => {
+//         glowInText.style.display = 'block';
+//         glowInText.style.visibility = 'visible';
+//         let letters = glowInText.textContent.split("");
+//         glowInText.textContent = "";
+
+//         let delay = 1200;
+//         if(glowInText.classList.contains('fast')) {
+//           delay = 0;
+//         }
+
+//         setTimeout(() => {
+//           letters.forEach((letter, i) => {
+//             const span = document.createElement("span");
+//             const br = document.createElement("br");
+
+//             if (letter === '$') {
+//               span.style.display = 'block';
+//               span.style.marginBottom = '10px';
+//               glowInText.append(span);
+//             } else {
+//               span.textContent = letter;
+//               span.style.animationDelay = `${i * 0.05}s`;
+//               glowInText.append(span);
+//               if (letter === ',' || letter === '.') {
+//                 glowInText.append(br);
+//               } 
+//             }
+            
+  
+//           });
+//         }, delay);
+//       });
+//     },1000);
+
+//     e.target.remove();
+//   });
+
+
+
+  
+
+// });
